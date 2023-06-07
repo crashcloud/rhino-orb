e68c4464eed7c6caf6d4ac471493122c8da1c569
Callum Sykes <28632338+clicketyclackety@users.noreply.github.com>
2023-06-06 17:25:05 -0700

    Updated params

--- src/scripts/download.sh
+++ src/scripts/download.sh
@@ -3,7 +3,6 @@
 VERSION=$(circleci env subst "${PARAM_VERSION}")
 FULLVERSION=$(circleci env subst "${PARAM_FULLVERSION}")
 CULTURE=$(circleci env subst "${PARAM_CULTURE}")
-TOKEN=$(circleci env subst "${PARAM_TOKEN}")
 
 python rhino-setup/py/download-rhino.py \
         -v "${VERSION}" \
--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-VERSION=$(circleci env subst "${PARAM_VERSION}")
-FULLVERSION=$(circleci env subst "${PARAM_FULLVERSION}")
-CULTURE=$(circleci env subst "${PARAM_CULTURE}")
 TOKEN=$(circleci env subst "${PARAM_TOKEN}")
 
 python rhino-setup/py/install-rhino.py \
