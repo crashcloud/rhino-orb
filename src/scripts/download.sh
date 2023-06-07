714ede779e763276e2ebdeb002239cecdd0021bd
Callum Sykes <28632338+clicketyclackety@users.noreply.github.com>
2023-06-06 18:32:09 -0700

    shug 2

--- src/scripts/download.sh
+++ src/scripts/download.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-VERSION=$(circleci env subst "${PARAM_VERSION}")
-FULLVERSION=$(circleci env subst "${PARAM_FULLVERSION}")
-CULTURE=$(circleci env subst "${PARAM_CULTURE}")
+VERSION=$(circleci env subst "$PARAM_VERSION")
+FULLVERSION=$(circleci env subst "$PARAM_FULLVERSION")
+CULTURE=$(circleci env subst "$PARAM_CULTURE")
 
-echo "${VERSION}"
-echo "${FULLVERSION}"
-echo "${CULTURE}"
+echo "$VERSION"
+echo "$FULLVERSION"
+echo "$CULTURE"
 
-python rhino-setup/py/download-rhino.py -v "${VERSION}" -fv "${FULLVERSION}" -c "${CULTURE}"
+python rhino-setup/py/download-rhino.py -v "$VERSION" -fv "$FULLVERSION" -c "$CULTURE"
