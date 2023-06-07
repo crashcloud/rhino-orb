09d292d53b8c43a286d45ec90df9fae7d9ee0284
Callum Sykes <28632338+clicketyclackety@users.noreply.github.com>
2023-06-06 18:38:58 -0700

    + more

---
+++ src/scripts/download.ps1
@@ -0,0 +1,16 @@
+#!powershell.exe -ExecutionPolicy Bypass
+
+Write-Host "This is powershell!"
+
+Write-Host $Env:PARAM_VERSION
+Write-Host $Env:VERSION
+
+VERSION=$(circleci env subst "$PARAM_VERSION")
+FULLVERSION=$(circleci env subst "$PARAM_FULLVERSION")
+CULTURE=$(circleci env subst "$PARAM_CULTURE")
+
+echo "$VERSION"
+echo "$FULLVERSION"
+echo "$CULTURE"
+
+python rhino-setup/py/download-rhino.py -v "$VERSION" -fv "$FULLVERSION" -c "$CULTURE"
