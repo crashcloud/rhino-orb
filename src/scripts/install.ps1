255b4425b662f0316574060c87c02c46826a4b6b
Callum Sykes <28632338+clicketyclackety@users.noreply.github.com>
2023-06-19 12:19:11 -0700

    Update install.ps1

--- src/scripts/install.ps1
+++ src/scripts/install.ps1
@@ -6,6 +6,11 @@ $token = [System.Environment]::GetEnvironmentVariable('RHINO_CORE_TOKEN')
 [System.Environment]::SetEnvironmentVariable('RHINO_TOKEN', $token, 'Machine')
 
 # Report the length 0 = not set
-[System.Environment]::GetEnvironmentVariable('RHINO_CORE_TOKEN').Length
+$len = [System.Environment]::GetEnvironmentVariable('RHINO_TOKEN').Length
+Write-Host $len
+if ($len == 0)
+{
+  [System.Environment]::SetEnvironmentVariable('RHINO_TOKEN', $token)
+}
 
 python rhino-setup/py/install-rhino.py -l CORE -tk $token
