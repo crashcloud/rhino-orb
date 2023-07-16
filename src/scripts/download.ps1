df7b70140abfce7708122cb03e9130e5d5157794
Callum Sykes <28632338+clicketyclackety@users.noreply.github.com>
2023-07-16 09:19:25 -0700

    Improved cache check

--- src/scripts/download.ps1
+++ src/scripts/download.ps1
@@ -6,5 +6,18 @@ Write-Host $ENV:PARAM_VERSION
 Write-Host $ENV:PARAM_FULLVERSION
 Write-Host $ENV:PARAM_CULTURE
 
+$version_file = "rhino_$ENV:PARAM_VERSION`_$ENV:PARAM_FULLVERSION"
+try
+{
+    $no_rhino = (Get-ChildItem $version_file).Length -eq 0
+    if ($no_rhino)
+    {
+        Write-Host "Cache for $version_file.exe was found, skipping download."
+        exit 0
+    }
+} catch {}
+
+Write-Host "No Cache found, Downloading!"
+
 ## https://github.com/mcneel/compute.rhino3d/blob/7.x/script/update-rhino.ps1
 python rhino-setup/py/download-rhino.py -v $ENV:PARAM_VERSION -fv $ENV:PARAM_FULLVERSION -c $ENV:PARAM_CULTURE
